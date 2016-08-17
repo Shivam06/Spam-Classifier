1af3be2ed5a151e9fc8adb9b054d8f88fe044c8d
Shivam06 <shivammahajan6596@gmail.com>
2016-08-17 22:09:09 +0530

    Script to chose best value of hyperparamater C.

---
+++ Model/ParametersLinsvm.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+def ParametersLinsvm(Xtrain,ytrain,Xval,yval):
+    
+    cvalue = [0.1,1,10]
+    max = 0    
+    scores = []
+    for c in cvalue:
+        svm = SVC(kernel = 'linear', random_state = 0, C=c)
+        svm.fit(Xtrain,ytrain)
+        ypred = svm.predict(Xval)
+        a = accuracy_score(ypred,yval)
+        scores.append(a)
+        if a > max:
+            max = a
+            C=c
+            
+            
+    return C,max,scores
